b19b76a9469438e8d57eed13e5405a068d65667f
Cheryet <C.heryet@outlook.com>
2022-11-15 21:25:16 -0700

    commit

--- _scripts/client.js
+++ _scripts/client.js
@@ -2,22 +2,33 @@
 $(document).ready(function() {
   console.log('Hello from doc ready!')
 
+
+  // Sticky nav bar on scroll
   var stickyOffset = $('#main-nav').offset().top;
 
   $(window).scroll(function(){
     let $navbar = $('#main-nav'),
-        scroll = $(window).scrollTop();
+    scroll = $(window).scrollTop();
   
     if (scroll >= stickyOffset) $navbar.addClass('sticky');
     else $navbar.removeClass('sticky');
   });
-  
-  
+
+  const $menuIcon = $('#menu-icon')
+  const $nav = $('#nav')
+
+  const menuHandler = function () {
+    $nav.slideToggle('slow')
+
+  }
 
 
+  $menuIcon.on('click', menuHandler) 
 
 
 })
+
+
 
 
 
